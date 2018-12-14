e3620e7f387d9289b8fa55580a71f457b3ee8c5d
pavelp21 <pavelvp2182@yandex.ru>
2018-12-14 16:47:51 +0500

    Section answers and questions is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -3,42 +3,42 @@
 $(document).ready(function(){
 /* Работа формы */
 $(function () {
-    var check = $('.check', this),
-        email = $('.mail', this),
-        message = $('.alert_message', this);
-    $(".form_newsletter").on("submit", function () {
-        var check = $('.check', this),
-            message = $('.alert_message', this),
-            reNone = /.+/,
-            email = $('.mail', this),
-            button = $('.button_mail', this);
-        if (!email.val().match(reNone)) {
-            message.text('Введите email').slideDown(500);
-            return false;
-        }
-        if (!check.prop("checked")) {
-            check.next().css("color", "red");
-            check.next().children().css("color", "red");
-            message.text('Подтвердите соглашение').slideDown(500);
-            return false;
-        }
-        if (email.val() && check) {
-            // button.text('Отправляем...');
-            button.text('Отправляем...');
-            setTimeout(function() {
-                button.text('Отправлено!');
-            }, 400);
-        }
-    });
-    email.click(function () {
-        email.css("border", "none");
-        message.slideUp(500);
-    });
-    check.click(function () {
-        check.next().css("color", "#000");
-        check.next().children().css("color", "#000");
-        message.slideUp(500);
-    });
+	var check = $('.check', this),
+		email = $('.mail', this),
+		message = $('.alert_message', this);
+	$(".form_newsletter").on("submit", function () {
+		var check = $('.check', this),
+			message = $('.alert_message', this),
+			reNone = /.+/,
+			email = $('.mail', this),
+			button = $('.button_mail', this);
+		if (!email.val().match(reNone)) {
+			message.text('Введите email').slideDown(500);
+			return false;
+		}
+		if (!check.prop("checked")) {
+			check.next().css("color", "red");
+			check.next().children().css("color", "red");
+			message.text('Подтвердите соглашение').slideDown(500);
+			return false;
+		}
+		if (email.val() && check) {
+			// button.text('Отправляем...');
+			button.text('Отправляем...');
+			setTimeout(function() {
+				button.text('Отправлено!');
+			}, 400);
+		}
+	});
+	email.click(function () {
+		email.css("border", "none");
+		message.slideUp(500);
+	});
+	check.click(function () {
+		check.next().css("color", "#000");
+		check.next().children().css("color", "#000");
+		message.slideUp(500);
+	});
 });
-    /* Конец документа*/
+	/* Конец документа*/
 });
